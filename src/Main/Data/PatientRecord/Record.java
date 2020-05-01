b27ffba3ee4a58c8c024b97883061c45cf9521d3
ajifoster3 <ajifoster3@gmail.com>
2020-05-01 18:09:46 +0100

    Added Methods for the PatientRecordManager, Needs Filling

--- src/Main/Data/PatientRecord/Record.java
+++ src/Main/Data/PatientRecord/Record.java
@@ -22,4 +22,134 @@ public class Record {
     private MRC mrc;
     private MMS mms;
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getBreathing() {
+        return breathing;
+    }
+
+    public void setBreathing(String breathing) {
+        this.breathing = breathing;
+    }
+
+    public String getCirculation() {
+        return circulation;
+    }
+
+    public void setCirculation(String circulation) {
+        this.circulation = circulation;
+    }
+
+    public String getDurationOfSession() {
+        return durationOfSession;
+    }
+
+    public void setDurationOfSession(String durationOfSession) {
+        this.durationOfSession = durationOfSession;
+    }
+
+    public String getComprehensiveRehab() {
+        return comprehensiveRehab;
+    }
+
+    public void setComprehensiveRehab(String comprehensiveRehab) {
+        this.comprehensiveRehab = comprehensiveRehab;
+    }
+
+    public String getFitnessToParticipate() {
+        return fitnessToParticipate;
+    }
+
+    public void setFitnessToParticipate(String fitnessToParticipate) {
+        this.fitnessToParticipate = fitnessToParticipate;
+    }
+
+    public String getDelirium() {
+        return delirium;
+    }
+
+    public void setDelirium(String delirium) {
+        this.delirium = delirium;
+    }
+
+    public String getIpat() {
+        return ipat;
+    }
+
+    public void setIpat(String ipat) {
+        this.ipat = ipat;
+    }
+
+    public Boolean getFitForPhysio() {
+        return fitForPhysio;
+    }
+
+    public void setFitForPhysio(Boolean fitForPhysio) {
+        this.fitForPhysio = fitForPhysio;
+    }
+
+    public List<String> getPhysioInterventions() {
+        return physioInterventions;
+    }
+
+    public void setPhysioInterventions(List<String> physioInterventions) {
+        this.physioInterventions = physioInterventions;
+    }
+
+    public int getRassHigh() {
+        return rassHigh;
+    }
+
+    public void setRassHigh(int rassHigh) {
+        this.rassHigh = rassHigh;
+    }
+
+    public int getRassLow() {
+        return rassLow;
+    }
+
+    public void setRassLow(int rassLow) {
+        this.rassLow = rassLow;
+    }
+
+    public SOFA getSofa() {
+        return sofa;
+    }
+
+    public void setSofa(SOFA sofa) {
+        this.sofa = sofa;
+    }
+
+    public CPAx getCpax() {
+        return cpax;
+    }
+
+    public void setCpax(CPAx cpax) {
+        this.cpax = cpax;
+    }
+
+    public MRC getMrc() {
+        return mrc;
+    }
+
+    public void setMrc(MRC mrc) {
+        this.mrc = mrc;
+    }
+
+    public MMS getMms() {
+        return mms;
+    }
+
+    public void setMms(MMS mms) {
+        this.mms = mms;
+    }
+
+
+
 }
--- src/Main/PatientRecordManager.java
+++ src/Main/PatientRecordManager.java
@@ -1,11 +1,85 @@
 package Main;
 
+import Main.Data.PatientRecord.CPAx;
+import Main.Data.PatientRecord.MRC;
+import Main.Data.PatientRecord.Record;
+import Main.Data.PatientRecord.SOFA;
+
+import java.util.Date;
 import java.util.List;
 
 public class PatientRecordManager {
 
-    public void constructRecord(List<String> patientRecordHeaders, List<String> excelheaders){
+    public PatientRecordManager() {
+    }
+
+    public Record constructRecord(List<String> patientRecordHeaders, List<String> excelheaders){
+
+        Record record = new Record();
+
+        return record;
 
     }
+
+    private Date cleansedDate(){
+        return null;
+    }
+
+    private String cleansedBreathing(){
+        return null;
+    }
+
+    private String cleansedCirculation(){
+        return null;
+    }
+
+    private String cleansedDurationOfSession(){
+        return null;
+    }
+
+    private String cleansedComprehensiveRehab(){
+        return null;
+    }
+
+    private String cleansedFitnessToParticipate(){
+        return null;
+    }
+
+    private String cleansedDelirium(){
+        return null;
+    }
+
+    private String cleansedIpat(){
+        return null;
+    }
+
+    private Boolean cleansedFitForPhysio(){
+        return null;
+    }
+
+    private List<String> cleansedInterventions(){
+        return null;
+    }
+
+    private int cleansedRassHigh(){
+        return -1;
+    }
+
+    private int cleansedRassLow(){
+        return -1;
+    }
+
+    private SOFA cleansedSOFA(){
+        return null;
+    }
+
+    private CPAx cleansedCPAx(){
+        return null;
+    }
+
+    private MRC cleansedMRC(){
+        return null;
+    }
+
 
 }
